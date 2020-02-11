b105b490f25c7e9a97694dd00514902e4abe348b
jeff.yang <jfyoung@ms10.hinet.net>
2020-02-11 18:05:19 +0800

    add hide reply

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    #message = TextSendMessage(text=event.message.text)
+    #line_bot_api.reply_message(event.reply_token, message)
     print("Group_ID:" + event.source.group_id)
 
 
